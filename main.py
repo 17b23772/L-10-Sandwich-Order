1f6307e2b714052e1c2e24aa3094a281ff65ccee
Oliver Beach <17b23772@marling.school>
2020-09-30 10:10:57 +0000

    Pizza order Updated

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 #Pizza order
+total_order = 0
+print ("Choose Your Pizza Base. You Can Either Have Thick Or Thin.")
+pizza_base = input() 
+if:
+  input() = "Thick":
+  total_order = + 8.00
+else: 
+
+print ("\n")
+print ("How Large Would You Like Your Pizza? You Can Eiter have 8, 10, 12, 14 or 18")
+pizza_size = input()
+print ("\n")
+print ("Would You Like Cheese On Your Pizza")
+choice_cheese = input()
+print ("\n")
+print ("What Pizza Type Would You Like?")
+print ("You Can Choose From: Margherita, Vegetable, Vegan, Hawaiian or Meat Feast")
+pizza_topping = input()
+print ("\n")
+print ("And Finally, Do you have A Voucher Code? If You Do Then Enter It Now. If Not Put No")
+if:
+  input() = FunnyFriday
+  print ("Thanks! Thats Ten Percent Off!")
+  print (total_order)
+else:
+  input() = no
+  print(total_order)
+
+
