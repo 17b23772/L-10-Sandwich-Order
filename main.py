2cfbab67636cdf268ec3cea1724dcf291fb7fdf0
Oliver Beach <17b23772@marling.school>
2020-09-30 10:21:41 +0000

    Failed attempt at making the bill add up

--- main.py
+++ main.py
@@ -2,14 +2,17 @@
 total_order = 0
 print ("Choose Your Pizza Base. You Can Either Have Thick Or Thin.")
 pizza_base = input() 
-if:
-  input() = "Thick":
+if input() == "Thick":
   total_order = + 8.00
 else: 
-
+  total_order = + 10.00
 print ("\n")
 print ("How Large Would You Like Your Pizza? You Can Eiter have 8, 10, 12, 14 or 18")
 pizza_size = input()
+if pizza_size == "8":
+  total_order = + 0
+else: 
+  total_order = + 2
 print ("\n")
 print ("Would You Like Cheese On Your Pizza")
 choice_cheese = input()
@@ -18,13 +21,8 @@ print ("What Pizza Type Would You Like?")
 print ("You Can Choose From: Margherita, Vegetable, Vegan, Hawaiian or Meat Feast")
 pizza_topping = input()
 print ("\n")
-print ("And Finally, Do you have A Voucher Code? If You Do Then Enter It Now. If Not Put No")
-if:
-  input() = FunnyFriday
-  print ("Thanks! Thats Ten Percent Off!")
-  print (total_order)
-else:
-  input() = no
-  print(total_order)
+print("Thanks For Shopping With Us! Here Is The Bill") 
+print (total_order)
+
 
 
